ff2415e392deabae4147ae35c5d67ea08dae6e9f
SimonFalkner <76668461+SimonFalkner@users.noreply.github.com>
2022-02-22 13:03:58 +0100

    Add crrent location blob

--- app/src/main/java/com/example/elsmaps/MainActivity.java
+++ app/src/main/java/com/example/elsmaps/MainActivity.java
@@ -3,19 +3,44 @@ package com.example.elsmaps;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,29 +54,73 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
     private TextView xCoord,yCoord,missionName;
-    private Button btnOpenNavigation;
+    private Button btnOpenNavigation, btnClosePopUp;
+    FusedLocationProviderClient locationClient;
+    SupportMapFragment mapFragment;
+    private CameraPosition cameraPosition;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private final LatLng defaultLocation = new LatLng(48.34536,14.16017);
+    private static final int DEFAULT_ZOOM =15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean locationPermissionGranted;
 
+
+    private Location lastKnownLocation;
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Retrieve location and camera position from saved instance state.
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
         setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
+        LatLng currentLocation;
+        //Initialize fused location
+
+        //check permission
+
+
+
 
         Mission mission1 = new Mission("Mission1", 48.34536,14.16017);
         Mission mission2 = new Mission("Mission2", 48.35485,14.16251);
         missionList = new ArrayList<Mission>();
         missionList.add(mission1);
         missionList.add(mission2);
+        Mission mission3;
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        if(mMap!=null){
+            outState.putParcelable(KEY_CAMERA_POSITION, mMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+
+
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
-
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setTrafficEnabled(true);
         for (Mission mission:missionList
              ) {
             mMap.addMarker(new MarkerOptions()
@@ -70,17 +139,76 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 //                .setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
 //
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(48.34536,14.16017)));
+
+        getLocationPermission();
+        getDeviceLocation();
+        updateLocationUI();
+
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getDeviceLocation(){
+        try{
+            if(locationPermissionGranted){
+                Task<Location> locationResult= fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if(task.isSuccessful()){
+                            lastKnownLocation = task.getResult();
+                            if(lastKnownLocation!=null){
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            }
+                            else{
+
+                                mMap.moveCamera(CameraUpdateFactory
+                                        .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                            }
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+    private void getLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        if (requestCode
+                == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            }
+        }else{
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        updateLocationUI();
     }
 
     @Override
     public void onInfoWindowClick(@NonNull Marker marker) {
-        Toast.makeText(this, "Info window clicked",
-                Toast.LENGTH_SHORT).show();
-
         createNewDialog(marker);
 
     }
-
 
     public void createNewDialog(Marker marker){
         dialogBuilder=new AlertDialog.Builder(this);
@@ -90,7 +218,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         missionName=(TextView) missionPopUpView.findViewById(R.id.popup_name);
 
         btnOpenNavigation=(Button) missionPopUpView.findViewById(R.id.popup_NavButton);
-
+        btnClosePopUp=(Button) missionPopUpView.findViewById(R.id.popup_close);
         missionName.setText(marker.getTitle());
         LatLng markerPosition = marker.getPosition();
         xCoord.setText(String.valueOf(markerPosition.latitude));
@@ -105,6 +233,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 startNavigation(markerPosition);
             }
         });
+        btnClosePopUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
 
 
     }
@@ -115,4 +249,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapIntent.setPackage("com.google.android.apps.maps");
         startActivity(mapIntent);
     }
+    @SuppressLint("MissingPermission")
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+
 }
